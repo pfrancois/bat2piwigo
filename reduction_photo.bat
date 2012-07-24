d3ee27e240bdef2668217a1615a30183da074a0b
pfrancois <pfrancois_99@yahoo.fr>
2012-07-24 23:41:37 +0200

    rajout des " afin de gerer les nom avec espaces

--- reduction_photo.bat
+++ reduction_photo.bat
@@ -12,14 +12,14 @@ if not exist reduit md reduit
 cd reduit
 if not exist "%~n1" md "%~n1"
 cd ..
-if not exist "%~n1" md %~n1
+if not exist "%~n1" md "%~n1"
 
 echo reduction des photos
-nconvert -out jpeg -i -opthuff -o reduit\%~n1\%% -q 80 -ratio -rtype lanczos -opthuff -keepfiledate -buildexifthumb -resize 800 600 -rflag orient *.jpg
-nconvert -out jpeg -rmeta -keepfiledate -rexifthumb -opthuff -o %~n1\%%-th.jpg -i -q 60 -ratio -rtype lanczos -resize 144 144 -rflag orient *.jpg
+nconvert -out jpeg -i -opthuff -o "reduit\%~n1\%%" -q 80 -ratio -rtype lanczos -opthuff -keepfiledate -buildexifthumb -resize 800 600 -rflag orient *.jpg
+nconvert -out jpeg -rmeta -keepfiledate -rexifthumb -opthuff -o "%~n1\%%-th.jpg" -i -q 60 -ratio -rtype lanczos -resize 144 144 -rflag orient *.jpg
 rem on met les fichiers afin de d'eviter toute piratage plus tard
-copy "d:\bat\index_rep_photos_pwg.php" .\reduit\%~n1\index.php
-copy "d:\bat\index_rep_photos_pwg.php" .\%~n1\index.php
+copy "d:\bat\index_rep_photos_pwg.php" ".\reduit\%~n1\index.php"
+copy "d:\bat\index_rep_photos_pwg.php" ".\%~n1\index.php"
 echo %ERRORLEVEL%
 if %ERRORLEVEL% NEQ 0 goto fin
 :quasifin
