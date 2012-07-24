7ab93b1632629129b75b854687614200c818836f
pfrancois <pfrancois_99@yahoo.fr>
2012-07-24 03:53:44 +0200

    maj du script pour reduire les photos

--- reduction_photo.bat
+++ reduction_photo.bat
@@ -1,31 +1,24 @@
-rem echo off
-setlocal
-set "str1=@%1"
-set str | findstr "\^^^^^@">nul && (
-  set "str1=%str1:^^^^^@=@%"
-)
-if ["%str1:"=^^^^^@%"] NEQ "" (
-    %~d1
-    cd "%~pn1"
-)
+echo off
+ setlocal
+ set "str1=%cd%"
+ set str | findstr "\^^^^^@">nul && (
+   set "str1=%str1:^^^^^@=@%"
+ )
+ if ["%str1:"=^^^^^@%"] NEQ "" (
+     %~d1
+     cd "%~pn1"
+ )
 if not exist reduit md reduit
-cd reduit
-cd ..
-echo rotation 
-d:\bat\jhead -ft -autorot -exonly *.*
+if not exist "%~n1" md "%~n1"
 echo reduction des photos
-nconvert -out jpeg -i -opthuff -o reduit\%% -q 80 -ratio -rtype lanczos -opthuff -keepfiledate -buildexifthumb -resize 800 600 -rflag orient *.jpg
-
+nconvert -out jpeg -i -opthuff -o reduit\%~n1\%% -q 80 -ratio -rtype lanczos -opthuff -keepfiledate -buildexifthumb -resize 800 600 -rflag orient *.jpg
+nconvert -out jpeg -rmeta -keepfiledate -rexifthumb -opthuff -o %~n1\%%-th.jpg -i -q 60 -ratio -rtype lanczos -resize 144 144 -rflag orient *.jpg
 rem on met les fichiers afin de d'eviter toute piratage plus tard
-rem cd reduit
-rem if not exist thumbnail md thumbnail
-rem cd ..
-
-copy "d:\bat\index_rep_photos_pwg.php" .\index.php
-rem copy "index_rep_photos_pwg.php" .\thumbnail\index.php
-rem nconvert -out jpeg -rmeta -keepfiledate -rexifthumb -opthuff -o thumbnail\TN-%% -i -q 60 -ratio -rtype lanczos -resize 128 128 -rflag orient *.jpg
-
+copy "d:\bat\index_rep_photos_pwg.php" .\reduit\%~n1\index.php
+copy "d:\bat\index_rep_photos_pwg.php" .\%~n1\index.php
+echo %ERRORLEVEL%
 if %ERRORLEVEL% NEQ 0 goto fin
+:quasifin
 pause
 
 :fin
