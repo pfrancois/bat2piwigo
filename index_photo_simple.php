2bf7492219b27cf2adb851a4b2c27494afeaab11
pfrancois <pfrancois_99@yahoo.fr>
2012-07-24 23:32:37 +0200

    verif que le photo simple marche

--- index_photo_simple.php
+++ index_photo_simple.php
@@ -17,12 +17,11 @@ fortement inspire de phalbum http://www.phphq.net/scripts.php?script=phAlbum#phA
 */
 
 $album_title=basename(dirname(realpath($_SERVER["PHP_SELF"]))); //The name of this album
-$show_files=array('jpg','gif','png','jpeg'); //The array, only show these types of files.
+$show_files=array('jpg','gif','png','jpeg'); //The array, only show these types of files. case non sensitive
 $thumbname="thumbnail";//name of the folder of thumbnail
 $ignore_word='-hide'; //Hide files or folder with this string in the name. Example, mypicture-hide.jpg will not be shown.
 $table_cells=4; //How many images/folders in each row do you want? // Looks best with 3
 //We don't want ugly _'s or -'s to display with the file or folder names do we? No! So, lets take them out.
-
 $find=array('_','-');//liste des trucs a remplacer
 $replace=array(' ',' ');//liste des trucs remplaces
 
@@ -39,6 +38,7 @@ $bgcolor='#293134'; // Page background color.
 
 $image_border=2; //Do you want a border around the images? 1-10, number of pixels.
 $border_color='#000000'; // What color do you want the image border to be?
+$admin_enable=True;//si true, il y a la possiblite d'avoir une version admin qui affiche tous les repertoires et les fichiers.
 
 /*
 //================================================================================
@@ -56,45 +56,45 @@ $border_color='#000000'; // What color do you want the image border to be?
 <title><?php echo $album_title;?></title>
 <style type='text/css'>
 body{
-	background-color:<?php echo $bgcolor;?>;
-	font-family: <?php echo $text_face;?>;
-	font-size: <?php echo $text_size;?>pt;
-	color: <?php echo $text_color;?>;
+    background-color:<?php echo $bgcolor;?>;
+    font-family: <?php echo $text_face;?>;
+    font-size: <?php echo $text_size;?>pt;
+    color: <?php echo $text_color;?>;
 }
 
 a:link {
-	text-decoration:none;
-	color:<?php echo $link_color;?>;
+    text-decoration:none;
+    color:<?php echo $link_color;?>;
 }
 
 a:visited {
-	text-decoration:none;
-	color:<?php echo $link_color;?>;
+    text-decoration:none;
+    color:<?php echo $link_color;?>;
 }
 
 a:hover {
-	text-decoration:none;
-	color:<?php echo $link_hover;?>;
+    text-decoration:none;
+    color:<?php echo $link_hover;?>;
 }
 
 .error{
-	font-family: <?php echo $text_face;?>;
-	font-size: <?php echo $text_size;?>pt;
-	color: <?php echo $error_color;?>;
+    font-family: <?php echo $text_face;?>;
+    font-size: <?php echo $text_size;?>pt;
+    color: <?php echo $error_color;?>;
 }
 
 .image{
-	border:<?php echo $image_border;?>px solid <?php echo $border_color;?>;
+    border:<?php echo $image_border;?>px solid <?php echo $border_color;?>;
 }
 
 input {
-	font-family:<?php echo $text_face;?>;
-	font-size:<?php echo $text_size;?>px;
-	color:<?php echo $text_color;?>;
-	border:1px solid <?php echo $border_color;?>;
+    font-family:<?php echo $text_face;?>;
+    font-size:<?php echo $text_size;?>px;
+    color:<?php echo $text_color;?>;
+    border:1px solid <?php echo $border_color;?>;
 }
 hr {
-	color:<?php echo $text_color;?>;
+    color:<?php echo $text_color;?>;
 }
 </style>
 </head>
@@ -110,14 +110,6 @@ function search_replace(/*.string.*/$haystack, /*.string.*/$needles, /*.string.*
     @var $offsetint numero de depart
     
     recherche et remplace dans hackstack tous les elements de needles par replacement*/
-    if ($needles=="") {
-        global $find;
-        $needles=$find;
-    }
-    if ($replacement=="") {
-        global $replace;
-        $replacement=$replace;
-    }
     
     for($i=0;$i<count($needles);$i++) {
         $found = stripos($haystack, $needles[$i], $offset);
@@ -132,6 +124,8 @@ function baseurl(/*.string.*/$url){
     array_pop($temp);
     return implode("/",$temp);
 }
+
+
 //Makes the tables look nice and pretty.
 
 if($table_cells<6) {
@@ -152,57 +146,58 @@ $path=dirname(__FILE__);
 //each directory
 $base_url=baseurl($_SERVER["SCRIPT_NAME"]);
 echo '<div><a href="http://'.$_SERVER['SERVER_NAME'].baseurl($base_url).'"> dossier sup&eacute;rieur</a> </div>';
-$f=0;
-if $_GET['francois']=1 {$admin=true;} else {$admin=false;}
-if (count(glob($path."/*",GLOB_ONLYDIR))>0) {
+$f=0;/*variable de comptage du nombre de dossiers*/
+if (isset($_GET['francois']) and $_GET['francois']==1) {$admin=true;} else {$admin=false;}
+/*affichage des sous repertoires, 1 car il y a automatiquement au moins le repertoire des vignettes*/
+if (count(glob($path."/*",GLOB_ONLYDIR))>1) {
     echo('<table style="border:0px;" cellpadding="15" width="98%" align="center">'."\n");
     foreach (glob($path."/*",GLOB_ONLYDIR) as $file){
-        $flag=false;
+        $fin_ligne=false;//fin_ligne est la variable afin de savoir si on est a la fin d'une ligne car il faut gerer les lignes et le tableau
         if (is_int($f / $table_cells)) {
             echo("<tr>\n");
         }
+        //on gere les exceptions d'affichage
         if (basename($file)!=$thumbname and (stripos($file,$ignore_word)===false or $admin=true)) {
             $f++;
-            echo '<td><a href="http://'.$_SERVER['SERVER_NAME'].$base_url."/".basename($file).'">'.basename(search_replace($file)).'</a></td>';
+            echo '<td><a href="http://'.$_SERVER['SERVER_NAME'].basename($file).'">'.basename(search_replace($file,$find,$replace)).'</a></td>';
             if(is_int($f / $table_cells)) {
-                flag=true;
+                $fin_ligne=true;
                 echo "</tr>\n";
             } else {
                 echo '';
             }
         }
     }
-    if (!is_int($f / $table_cells) and $flag==false) {
+    if (!is_int($f / $table_cells) and $fin_ligne==false) {
             echo("</tr>\n");
     }
     echo "</table>\n";
-    echo "<hr size='1' />\n";
 }
-
+echo "<hr size='1' />\n";
 //Lets get some images!!
     
 //Loop through them all ;).
-$base_url="http://".$_SERVER['SERVER_NAME'].$base_url."/";
-$k=0;
+$base_url="http://".$_SERVER['SERVER_NAME'].$base_url."/";//url complete du site de base avant l'ajout de la reference de la photo
+$k=0;//numero de la photo
 $image_close=false;
-$images="";
-$thumb_error="";
+$images="";//chaine de caractere qui va contenir le html crée
+$thumb_error="";//cahine de gestion des erreurs
 foreach (glob($path."/*") as $file){
     //Don't display the stupid directory tree files.
-    $file_name=basename($file);
+    $file_name=basename($file);//nom fu fichier image
     if($file!= '.' and $file!='..' and is_dir($file)===false){
         $path_parts=pathinfo($file);
-        $file_ext=$path_parts['extension'];
-        $display_name=search_replace($file_name);
+        $file_ext=$path_parts['extension'];//extension du fichier image
+        $display_name=search_replace($file_name,$find,$replace);//legende de l'image
         //Hide the thumb files from displaying as regular files and disallow any file types that are not allowed.
-        if(in_array($file_ext,$show_files) and stripos($file,$ignore_word)===false) {
+        //if the file has the ignore word in it, do not show the file.
+        if(in_array(strtolower($file_ext),$show_files) and stripos($file,$ignore_word)===false) {
             if(is_int($k / $table_cells) and $k!=0) {
                 $images .= "<tr>\n";
             } else {
                 $images .='';
             }  
-            //if the file has the ignore word in it, do not show the file.
-            //if a thumb file dosen't exists, then try and make one.
+            //if a thumb file dosen't exists, then report.
             if(!file_exists($path.'/'.$thumbname.'/TN-'.$file_name)) {
                 $thumb_error .='Thumb for '.$file.' not created.<br />';
                 $thumb=$base_url.'/'.$file_name;
@@ -223,26 +218,26 @@ foreach (glob($path."/*") as $file){
         }
     }
 }
-
+//si il y a des images on lance le rendering html
 if($images) {
-	echo("<table style='border:0px;' cellpadding='15' width='98%' align='center'>\n");
-	echo("<tr>\n");
-	echo($images);
-	if(!is_int($k / $table_cells)) {
-		echo("</tr>\n");
-	}
-	echo("</table>\n");
+    echo("<table style='border:0px;' cellpadding='15' width='98%' align='center'>\n");
+    echo("<tr>\n");
+    echo($images);
+    if(!is_int($k / $table_cells)) {
+        echo("</tr>\n");
+    }
+    echo("</table>\n");
 
-	if($thumb_error && !$supress_error) {
-		echo("<div align='center'><span class='error'>The following thumb errors have occured:</span><br /><br /> <b>".$thumb_error."</div>");
-	}
+    if($thumb_error && !$supress_error) {
+        echo("<div align='center'><span class='error'>The following thumb errors have occured:</span><br /><br /> <b>".$thumb_error."</div>");
+    }
 
-} else {
-	echo("<table style='border:0px;width:100%;' cellpadding='15' align='center'>\n");
-	echo("<tr>\n");
-	echo("<td align='center'><b>No images to display in this album. Please pick another album.</b></td>");
-	echo("</tr>\n");
-	echo("</table>\n");
+} else {//sion pas d'images
+    echo("<table style='border:0px;width:100%;' cellpadding='15' align='center'>\n");
+    echo("<tr>\n");
+    echo("<td align='center'><b>No images to display in this album. Please pick another album.</b></td>");
+    echo("</tr>\n");
+    echo("</table>\n");
 }
 ?>
 </body>
